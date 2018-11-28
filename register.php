9a88787009a410c6428b5dad7ab80d674445dfa6
Puneet Pareenja <pareenjapuneet@gmail.com>
2018-11-28 00:39:03 -0800

    Added register.php

--- register.php
+++ register.php
@@ -2,15 +2,15 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Recipe Manager</title>
+    <title>Create your Recipe Manager Account</title>
   </head>
   <body>
     <form class="" action="" method="post">
       <input type="text" name="usernameText" placeholder="Username">
+      <input type="email" name="emailText" placeholder="Email">
       <input type="password" name="password" placeholder="Password">
-      <button type="submit" name="signInButton">Sign In</button>
-      <a href="#">Create Account</a>
-      <a href="#">Forgot Password?</a>
+      <input type="password" name="password2" placeholder="Confirm Password" >
+      <button type="submit" name="submit">Register</button>
     </form>
   </body>
 </html>
