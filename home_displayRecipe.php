22df3c9f583449f68fc57894074c3526bf62a4b9
Puneet Pareenja <pareenjapuneet@gmail.com>
2018-11-30 15:23:16 -0800

    Clearing some space

--- home_displayRecipe.php
+++ home_displayRecipe.php
@@ -7,17 +7,17 @@
   <body>
   	<?php
 	require "recipemanagementdb_connect.php";
-	
+
 	global $dbConn;
 	$recipeName = "SELECT * FROM recipe";
-		
+
 	try{
 		//prepare query statement
-		$displayRecipe=$dbConn->prepare($recipeName);	
-		//execute query	
-		$displayRecipe->execute();	
+		$displayRecipe=$dbConn->prepare($recipeName);
+		//execute query
+		$displayRecipe->execute();
 		//fetcch query results
-		$recipeTable=$displayRecipe->fetchall(PDO::FETCH_ASSOC);		
+		$recipeTable=$displayRecipe->fetchall(PDO::FETCH_ASSOC);
 		//release db connector Cursor
 		$displayRecipe -> CloseCursor();
 
@@ -25,8 +25,8 @@
 	catch(PDOException $ex){
 		//get error Message
 		$ex -> getMessage();
-	}			
-	
+	}
+
 	print "<center>
 			<table border=1>
 			<tr>
@@ -36,37 +36,37 @@
 			<th>Cook Time</th>
 			<th>Author Name</th>
 			</tr>";
-	
-	$countAmt = count($recipeTable);		
+
+	$countAmt = count($recipeTable);
 	for($i = 0; $i<$countAmt ; $i++){
 		$authorID = $recipeTable[$i]['AuthorID'];
 		$authorName = "SELECT AuthorName FROM author WHERE AuthorID IN(SELECT AuthorID FROM recipe WHERE AuthorID = '$authorID')";
-		
+
 		try{
-			$displayAuthor=$dbConn->prepare($authorName);	
-			$displayAuthor->execute();	
-			$recipeAuthor=$displayAuthor->fetchall(PDO::FETCH_ASSOC);	
-			$displayAuthor -> CloseCursor();			
+			$displayAuthor=$dbConn->prepare($authorName);
+			$displayAuthor->execute();
+			$recipeAuthor=$displayAuthor->fetchall(PDO::FETCH_ASSOC);
+			$displayAuthor -> CloseCursor();
 		}
 		catch(PDOException $ex){
 			$ex -> getMessage();
 		}
-		
+
 		print  "<tr><td>".$recipeTable[$i]['RecipeName']."</td>
 				<td>".$recipeTable[$i]['RecipeDesc']."</td>
 				<td>".$recipeTable[$i]['PrepTime']."</td>
 				<td>".$recipeTable[$i]['CookTime']."</td>
 				<td>".$recipeAuthor['0']['AuthorName']."</td></tr>";
-	}	
-			
+	}
+
 	print   "</tr></table></center>";
 
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
 		//print "<pre>";
 		//print $authorID;
 		//print_r($recipeAuthor);
