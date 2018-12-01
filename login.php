a7cdf7029ba4657a7da5c4c0d591277ebcf47632
Puneet Pareenja <pareenjapuneet@gmail.com>
2018-12-01 00:53:18 -0800

    Added master.css, reset.css and changed the login page

--- login.php
+++ login.php
@@ -2,15 +2,56 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Recipe Manager</title>
+    <link rel="stylesheet" href="/css/reset.css">
+    <link rel="stylesheet" href="css/material.css">
+    <link rel="stylesheet" href="/css/master.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <script src="js/material.js"></script>
   </head>
   <body>
-    <form class="" action="" method="post">
-      <input type="text" name="usernameText" placeholder="Username">
-      <input type="password" name="password" placeholder="Password">
-      <button type="submit" name="signInButton">Sign In</button>
-      <a href="#">Create Account</a>
-      <a href="#">Forgot Password?</a>
-    </form>
+    <div class="mdl-layout mdl-js-layout mdl-color--grey-100">
+      <main class="mdl-layout__content">
+        <div class="mdl-card mdl-shadow--6dp">
+          <div class="mdl-card__title mdl-color--primary mdl-color-text--white">
+            <h2 class="mdl-card__title-text">Acme Co.</h2>
+          </div>
+          <div class="mdl-card__supporting-text">
+            <form action="#">
+              <div class="mdl-textfield mdl-js-textfield">
+                <input class="mdl-textfield__input" type="text" id="username" />
+                <label class="mdl-textfield__label" for="username">Username</label>
+              </div>
+              <div class="mdl-textfield mdl-js-textfield">
+                <input class="mdl-textfield__input" type="password" id="userpass" />
+                <label class="mdl-textfield__label" for="userpass">Password</label>
+              </div>
+            </form>
+          </div>
+          <div class="mdl-card__actions mdl-card--border">
+            <button class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">Log in</button>
+          </div>
+        </div>
+      </main>
+    </div>
+    <!-- <center>
+      <form action="#" action="" method="post">
+        <div class="demo-card-wide mdl-card mdl-shadow--2dp">
+          <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+            <input class="mdl-textfield__input" type="text" id="usernameText" name="usernameText">
+            <label class="mdl-textfield__label" for="usernameText">Username</label>
+          </div>
+          <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+            <input class="mdl-textfield__input" type="password" id="password" name="password">
+            <label class="mdl-textfield__label" for="password">Password</label>
+          </div>
+          <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" type="submit" name="signInButton">Sign In</button>
+          <a href="#">Create Account</a>
+          <a href="#">Forgot Password?</a>
+        </div>
+      </form>
+    </center> -->
   </body>
+
 </html>
