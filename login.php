434aa493c9662c74c431a6112c8e16ef7953b12d
JanuLiu <44848255+JanuLiu@users.noreply.github.com>
2018-11-30 18:51:49 -0800

    Add files via upload

--- login.php
+++ login.php
@@ -1,5 +1,8 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
+<?php
+	require "recipemanagementdb_connect.php";
+?>
   <head>
     <meta charset="utf-8">
     <title>Recipe Manager</title>
@@ -8,7 +11,65 @@
     <form class="" action="" method="post">
       <input type="text" name="usernameText" placeholder="Username">
       <input type="password" name="password" placeholder="Password">
-      <button type="submit" name="signInButton">Sign In</button>
+      <button type="submit" name="submit">Sign In</button>
+      <?php
+      if(isset($_POST['submit'])){
+      	global $dbConn;
+	  	$userName = $_POST['usernameText'];
+	  	$userPassword = $_POST['password'];
+	  	
+	  	$UserNameQuery = "SELECT AuthorName FROM author WHERE AuthorName = '$userName'";
+	  	$UserPasswordQuery = "SELECT AuthorPassword FROM author WHERE AuthorName = '$userName'";
+	  	try{
+			$checkUserName = $dbConn->prepare($UserNameQuery);
+			$checkUserPassword = $dbConn->prepare($UserPasswordQuery); 	
+			$checkUserName->execute();
+			$checkUserPassword->execute();
+			$resultUserName = $checkUserName->fetchall(PDO::FETCH_ASSOC);
+			$resultUserPassword = $checkUserPassword->fetchall(PDO::FETCH_ASSOC);
+			$checkUserName -> CloseCursor();
+			$checkUserPassword -> CloseCursor();
+			
+			/*print "<pre>";
+			print_r($resultUserName);
+			print "</pre>";
+			print "<pre>";
+			print_r($resultUserPassword);
+			print "</pre>";*/
+			
+			
+			if($userName!=null && $userPassword!=null){
+				if($resultUserName!=null&&$resultUserPassword!=null){
+					if($resultUserPassword[0]['AuthorPassword']==$userPassword){
+						print "Good to go";
+						header('Location: home_displayRecipe.php');
+						
+					}
+					else{
+						Print "Password Wrong~";
+					}
+				}
+				else{
+					print "You haven't set up your account or password before, <a href='#'> click here </a> to sing up!";
+				}
+			}
+			else{
+				print "Please enter your account and password! Thanks!";
+			}			
+		}
+		catch(PDOExcpetion $ex){
+			print "Failed";
+		}
+	  }
+      
+      
+      
+ 
+      
+      ?>
+      
+      
+      
       <a href="#">Create Account</a>
       <a href="#">Forgot Password?</a>
     </form>
--- register.php
+++ register.php
@@ -1,5 +1,8 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
+<?php
+	require "recipemanagementdb_connect.php";
+?>
   <head>
     <meta charset="utf-8">
     <title>Create your Recipe Manager Account</title>
@@ -11,6 +14,64 @@
       <input type="password" name="password" placeholder="Password">
       <input type="password" name="password2" placeholder="Confirm Password" >
       <button type="submit" name="submit">Register</button>
+      <?php
+		if(isset($_POST['submit'])){
+			global $dbConn;
+	  		$userName = $_POST['usernameText'];
+	  		$userEmail = $_POST['emailText'];
+	  		$userPassword = $_POST['password'];
+	  		$userPasswordCon = $_POST['password2'];
+	  	
+	  		if($userName!=null&&$userEmail!=null&&$userPassword!=null&&$userPasswordCon!=null){
+	  			if($userPassword == $userPasswordCon){
+					$InsertUserQuery = "INSERT INTO author (AuthorName,AuthorEmail,AuthorPassword) VALUES ('$userName','$userEmail','$userPassword')";
+					try{					 	
+						$checkInsertUserName = $dbConn->prepare($InsertUserQuery);
+						$checkInsertUserName->execute();
+						print "Good to go! Please sign in!";
+						header('Location: login.php');
+					}
+					catch(PDOExcpetion $ex){
+						print "Failed";
+					}
+				}
+				else{
+					print "Please confirm your password";
+				}
+			}
+			else{
+				print "Please enter your name, email address and password, thanks!";
+			}	  	
+	  	}
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+           /*
+        DROP TABLE IF EXISTS AUTHOR;
+		CREATE TABLE IF NOT EXISTS AUTHOR(
+   		AuthorID   INTEGER  NOT NULL PRIMARY KEY AUTO_INCREMENT
+  		,AuthorName VARCHAR(20) NOT NULL
+  		,AuthorEmail VARCHAR(50) NOT NULL
+  		,AuthorPassword VARCHAR(20) NOT NULL
+		);
+		INSERT INTO AUTHOR(AuthorID,AuthorName,AuthorEmail,AuthorPassword) VALUES (200,'Alexis','alexis@gmail.com','alexis1234');
+	INSERT INTO AUTHOR(AuthorID,AuthorName,AuthorEmail,AuthorPassword) VALUES (201,'Judy Diercks Oreilly','judy@gmail.com','judy1234');
+	INSERT INTO AUTHOR(AuthorID,AuthorName,AuthorEmail,AuthorPassword) VALUES (202,'Soup Loving Nicole','soup@gmail.com','soup1234');
+	INSERT INTO AUTHOR(AuthorID,AuthorName,AuthorEmail,AuthorPassword) VALUES (203,'Andrea Cushman','andrea@gmail.com','andrea1234');
+	INSERT INTO AUTHOR(AuthorID,AuthorName,AuthorEmail,AuthorPassword) VALUES (204,'VBRAUER671','vbrauer671@gmail.com','vbrauer671');
+	INSERT INTO AUTHOR(AuthorID,AuthorName,AuthorEmail,AuthorPassword) VALUES (205,'Sean S.','sean@gmail.com','sean1234');*/
+      
+      
+      
+      
+      ?>
     </form>
   </body>
 </html>
