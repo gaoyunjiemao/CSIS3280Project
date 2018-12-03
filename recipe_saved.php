0f11a18eab2a56c37df89a535c09edced385be79
gaoyunjiemao <gaoyunjiemao@gmail.com>
2018-12-03 09:23:08 +0800

    Add files via upload

---
+++ recipe_saved.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title></title>
+	</head>
+	<body>
+		<?php
+		print "<p>Your Recipe has been saved successfull!</p><br />";
+		print "<a href='index.php'>Click Here to back to home page</a>";
+		?>
+	</body>
+</html>
