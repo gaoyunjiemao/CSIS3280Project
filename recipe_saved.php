6f23267babc08ea713859dcb8681b70b0f2cd2f1
Puneet Pareenja <pareenjapuneet@gmail.com>
2018-12-03 03:31:02 -0800

    Update recipe_saved.php design

--- recipe_saved.php
+++ recipe_saved.php
@@ -3,11 +3,31 @@
 	<head>
 		<meta charset="utf-8">
 		<title></title>
+		<link rel="stylesheet" href="css/reset.css">
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto" >
+		<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+		<link rel="stylesheet" href="css/materialize.css">
+		<link rel="stylesheet" href="css/master.css" />
 	</head>
 	<body>
-		<?php
-		print "<p>Your Recipe has been saved successfully!</p><br />";
-		print "<a href='index.php'>Click Here to back to home page</a>";
-		?>
+		<div class="container">
+			<?php
+				include "header.php";
+			?>
+			<div class="container">
+				<div class="card">
+					<div class="card-content">
+						<div class="card-title center">Recipe Saved</div>
+						<div class="card-content center"><p>Your Recipe has been saved successfully!</p><br />
+							<a href='index.php'>Click Here to back to home page</a></div>
+					</div>
+				</div>
+			</div>
+			<!-- print "";
+			print "<a href='index.php'>Click Here to back to home page</a>"; -->
+			<?php
+				include "footer.php";
+			?>
+		</div>
 	</body>
 </html>
